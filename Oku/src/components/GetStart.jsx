5eacabc8a00ebbd589b5f4d13c755518e56849bb
Samba <danielsamba0000@icloud.com>
2026-08-07 12:08:34 +0100

    lettermissing

--- Oku/src/components/GetStart.jsx
+++ Oku/src/components/GetStart.jsx
@@ -1,11 +1,56 @@
-import model from "../assets/OkuModel.gif" 
-export default function GetStarted () { 
-    return (
-        <div className="started">
-<div></div>
-<div><img src={model} alt="Get Started" /></div>
-        </div>
-    )
-}
+import model from "../assets/machine.png";
 
-//src\assets\OkuModel.gif
+export default function GetStarted() {
+  return (
+    <section className="started">
+
+      <div className="startedtext">
+
+        <h1 className="hero-title">
+          Automatize.
+          <br />
+          Integre.
+          <br />
+          <span>Transforme.</span>
+        </h1>
+
+        <p className="hero-description">
+          OkuTokeka ajuda sua empresa a conectar sistemas,
+          automatizar processos e tomar decisões inteligentes
+          com tecnologia moderna e escalável.
+        </p>
+
+        <div className="hero-buttons">
+          <button className="btn-primary">
+            Agendar uma demo
+          </button>
+
+          <button className="btn-secondary">
+            Fale Conosco
+          </button>
+        </div>
+
+        <div className="hero-companies">
+          <p>Confiado por empresas inovadoras</p>
+
+          <div className="companies">
+            <span>TechNova</span>
+            <span>InovaHub</span>
+            <span>DataCore</span>
+            <span>NextGen</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="modelmachinecenter">
+        <img
+          src={model}
+          alt="Tecnologia OkuTokeka"
+          className="modelmachineimag"
+        />
+      </div>
+
+    </section>
+  );
+}
--- Oku/src/components/Header.jsx
+++ Oku/src/components/Header.jsx
@@ -1,22 +1,16 @@
+import logo from '../assets/removed.png';
 export default function Header() {
 
     return (
         <header className="header">
-            <div className="logo">OkuTokeka</div>
+            <div className="logo"><img src={logo} alt="OkuTokeka Logo" className='logoimg' />OkuTokeka</div>
             <div className="navigation">
                 <ol className="nav-list">
-                    <ul>Home</ul>
-                    <ul>Products</ul>
-                    <ul>Slutions</ul>
-                    <ul>About</ul>
-                    <ul>Carrees</ul>
-                    <ul>Contact</ul>
-                    <ul>Sign In</ul>
-                    <ul>Sign Up</ul>
+          
                 </ol>
             </div>
             <div className="get-started">
-                <button>Get Started</button>
+                <button className="btn-thrid">Secret Acess</button>
             </div>
         </header>
     )}
